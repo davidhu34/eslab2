ec9506993808af5a452acd91b6a195b2dfed6987
davidhu34 <b01901133@gmail.com>
2015-11-08 17:40:05 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,32 +7,71 @@ var sanitizeHtml = require('sanitize-html');
 var db = require('../db');
 
 router.get('/', function(req, res) {
-  res.render('index');
+	res.render('index');
 });
 
 router.get('/data', function(req, res) {
-  res.json({
-    xyz: [
-      [1, 2, 3],
-      [1, 2, 4],
-      [1, 2, 7]
-    ]
-  });
+
+	db.tessel.find({}, function(err, doc){
+		if(!err)
+			res.json(doc);
+	});
 });
 
-router.get('/tessel/accelerometer', function(req, res) {
-
+router.post('/tessel/accelerometer', function(req, res) {
+	res.send(req.body);
+	console.log(req.body);
+	req.body.data.map(function(d){
+		var data = {
+			type: 'accelerometer',
+			data: d,
+			timestamp: Date.now()
+		};
+		db.tessel.insert(data);
+	});
 });
 
 router.post('/tessel/climate', function(req, res) {
-
+	res.send(req.body);
+	req.body.data.map(function(d){
+		var pair = [];
+		pair.push(d.t);
+		pair.push(d.h);
+		var data = {
+			type: 'climate',
+			data: pair,
+			timestamp: Date.now()
+		};
+		db.tessel.insert(data);
+	});
 });
-
 router.post('/tessel/ambient', function(req, res) {
-
+	res.send(req.body);
+	req.body.data.map(function(d){
+		var pair = [];
+		pair.push(d.l);
+		pair.push(d.n);
+		var data = {
+			type: 'ambient',
+			data: pair,
+			timestamp: Date.now()
+		};
+		db.tessel.insert(data);
+	});
 });
 router.post('/tessel/gps', function(req, res) {
-
+	res.send(req.body);
+	req.body.data.map(function(d){
+		var pair = [];
+		pair.push(d.lon);
+		pair.push(d.lat);
+		var data = {
+			type: 'gps',
+			data: pair,
+			timestamp: Date.now()
+		};
+		db.tessel.insert(data);
+	});
 });
 
 router.io = io;
